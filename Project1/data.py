fcd95d276c8d77e0c7f3276c8cc4968c9decc92c
Kacper Grzymkowski <kacper.grzymkowski00@gmail.com>
2023-03-20 13:59:38 +0100

    Update data.py

--- Project1/data.py
+++ Project1/data.py
@@ -172,7 +172,7 @@ def augmented_cifar10_dataset_gauss_noise(variance, bs=16):
 def augmented_cifar10_dataset_randomflip_rotate_randomapply(rotate, bs=16):
     transform = torchvision.transforms.Compose([
         torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         torchvision.transforms.RandomHorizontalFlip(),
         torchvision.transforms.RandomApply(transforms=[torchvision.transforms.RandomRotation(rotate)], p=0.25),
     ])
